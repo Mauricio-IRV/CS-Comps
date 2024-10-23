d80c2fb193cb0b7a478f58708cd67d29236165f6
TetraGenesis <santmonbarb@gmail.com>
2024-10-23 12:08:17 -0500

    some packaging stuff

--- prototype/__init__.py
+++
@@ -1,8 +0,0 @@
-from arpspoof import ArpSpoofer
-from cSubprocess import *
-from bettercap import run_bettercap
-from exporting import writeCapture, displayCapture
-from main import main
-from networking import enable_ip_forwarding, get_default_gateway_ip, get_mac_address, ping_address
-from sslStripping import ssl_strip
-from dos import dos
---
+++ prototype/mainpackage/__init__.py
@@ -0,0 +1,8 @@
+from .arpspoof import ArpSpoofer
+from .cSubprocess import clean_subprocess
+from .bettercap import run_bettercap
+from .exporting import writeCapture, displayCapture
+from .main import main
+from .networking import enable_ip_forwarding, get_default_gateway_ip, get_mac_address, ping_address
+from .sslStripping import ssl_strip
+from dos import dos
