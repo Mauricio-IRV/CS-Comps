a3e510abed4855a06d27df89ae2c272297a6e1df
TetraGenesis <santmonbarb@gmail.com>
2024-10-25 12:06:49 -0500

    New build with successful package imports

--- prototype/mainpackage/__init__.py
+++ prototype/mainpackage/__init__.py
@@ -3,7 +3,7 @@ from .cSubprocess import clean_subprocess
 from .bettercap import run_bettercap
 from .exporting import writeCapture, displayCapture
 from .main import main
-from .networking import enable_ip_forwarding, get_default_gateway_ip, get_mac_address, ping_address
+from .networking import set_ip_forwarding, get_default_gateway_ip, get_mac_address, ping_address
 from .sslStripping import ssl_strip
-from dos import dos
-from packetManip import Raw
+from .dos import dos
+from .packetManip import Raw
